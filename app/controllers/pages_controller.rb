f2339b9f1cfe10bfff647c26656999024d3f2f83
Aidi Johari <aidijohari@gmail.com>
2021-03-10 18:32:52 +0800

    test remove code routes,pages/home

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -7,21 +7,21 @@ class PagesController < ApplicationController
   end
 
   def search
-    if params[:search].present? && params[:search].strip != ""
-      session[:loc_search] = params[:search]
-    end
+    # if params[:search].present? && params[:search].strip != ""
+    #   session[:loc_search] = params[:search]
+    # end
 
-    if session[:loc_search] && session[:loc_search] != ""
-      @venues_address = Venue.where(active: true).near(session[:loc_search], 5, order: "distance")
-      @venues_all = Venue.where(active: true).all - @venues_address
-    else
-      @venues_address = Venue.where(active: true).all
-    end
+    # if session[:loc_search] && session[:loc_search] != ""
+    #   @venues_address = Venue.where(active: true).near(session[:loc_search], 5, order: "distance")
+    #   @venues_all = Venue.where(active: true).all - @venues_address
+    # else
+    #   @venues_address = Venue.where(active: true).all
+    # end
 
-    @search = @venues_address.ransack(params[:q])
-    @venues = @search.result
+    # @search = @venues_address.ransack(params[:q])
+    # @venues = @search.result
 
-    @arrVenues = @venues.to_a
+    # @arrVenues = @venues.to_a
 
   end
 
