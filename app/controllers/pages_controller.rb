973059014c631893f2db0ce86021b43ad163f39c
Aidi Johari <aidijohari@gmail.com>
2021-03-11 11:08:16 +0800

    Testing digitalocean run

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,6 +1,6 @@
 class PagesController < ApplicationController
   def home
-    @venues = Venue.where(active: true).limit(3)
+    # @venues = Venue.where(active: true).limit(3)
   end
 
   def tos
--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,7 @@ Rails.application.routes.draw do
 
   root 'pages#home'
 
-  get 'pages/home'
+  # get 'pages/home'
   
   devise_for  :users,
               path: '',
