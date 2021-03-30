d5d815e51a0db40980150c3f071343dbc73dfadf
Aidi Johari <aidijohari@gmail.com>
2021-03-30 15:40:26 +0800

    gravatar

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -5,7 +5,7 @@ module ApplicationHelper
         user.image
       else
         gravatar_id = Digest::MD5::hexdigest(user.email).downcase
-        "https://www.gravatar.com/avatar/#{gravatar_id}.jpg?d=identical&s=150"
+        "https://www.gravatar.com/avatar/#{gravatar_id}.jpg"
       end 
     end
 
