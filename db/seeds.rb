7335db7e27cf1bcdba7322069dc333ba5a45b4a8
Aidi Johari <aidijohari@gmail.com>
2021-03-30 14:17:26 +0800

    credentials upd

--- db/seeds.rb
+++ db/seeds.rb
@@ -9,12 +9,14 @@
 Venue.destroy_all
 
 50.times do |i|
+    google_maps_api_key = "AIzaSyCpRjVymCuOdhVvvbECfZFgvRjv7MpXvaw"
+
     venue = Venue.create!(
         id: Faker::Number.unique.between(from: 1, to: 50),
         venue_type: ['Hotel', 'House', 'Hall'].sample(1),
         listing_name: Faker::JapaneseMedia::DragonBall.character,
         summary: Faker::Lorem.sentence(word_count: 3, supplemental: true),
-        address: Faker::Address.full_address,
+        # address: Faker::Address.full_address,
         price: Faker::Number.within(range:10..2000),
         active: "true",
         user_id: "1",
